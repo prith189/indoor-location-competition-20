3a1467de20504b300e9a7b483a4a8b1f595145d3
Prithvi N <prith189@gmail.com>
2021-02-02 22:59:29 -0800

    Custom wifi read

--- io_f.py
+++ io_f.py
@@ -16,6 +16,55 @@ class ReadData:
     ibeacon: np.ndarray
     waypoint: np.ndarray
 
+def read_data_file_wifi(data_filename):
+    acce = []
+    acce_uncali = []
+    gyro = []
+    gyro_uncali = []
+    magn = []
+    magn_uncali = []
+    ahrs = []
+    wifi = []
+    ibeacon = []
+    waypoint = []
+
+    with open(data_filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line_data in lines:
+        line_data = line_data.strip()
+        if not line_data or line_data[0] == '#':
+            continue
+
+        line_data = line_data.split('\t')
+
+        
+
+        if line_data[1] == 'TYPE_WIFI':
+            sys_ts = line_data[0]
+            ssid = line_data[2]
+            bssid = line_data[3]
+            rssi = line_data[4]
+            lastseen_ts = line_data[6]
+            wifi_data = [sys_ts, ssid, bssid, rssi, lastseen_ts]
+            wifi.append(wifi_data)
+            continue
+
+        if line_data[1] == 'TYPE_WAYPOINT':
+            waypoint.append([int(line_data[0]), float(line_data[2]), float(line_data[3])])
+
+    acce = np.array(acce)
+    acce_uncali = np.array(acce_uncali)
+    gyro = np.array(gyro)
+    gyro_uncali = np.array(gyro_uncali)
+    magn = np.array(magn)
+    magn_uncali = np.array(magn_uncali)
+    ahrs = np.array(ahrs)
+    wifi = np.array(wifi)
+    ibeacon = np.array(ibeacon)
+    waypoint = np.array(waypoint)
+
+    return ReadData(acce, acce_uncali, gyro, gyro_uncali, magn, magn_uncali, ahrs, wifi, ibeacon, waypoint)
 
 def read_data_file(data_filename):
     acce = []
